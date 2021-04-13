323d542e229a6a7636aab10e6a5a21d532d4f5e8
PankornSupercool__ <77049442+pankornsupercool@users.noreply.github.com>
2021-04-13 23:29:44 +0700

    Json added

--- real-implement/src/router/index.js
+++ real-implement/src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
-
+import EventList from "../views/EventList.vue";
+import About from "../views/About.vue";
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "EventList",
+    component: EventList,
   },
   {
     path: "/about",
@@ -13,9 +13,8 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+    component: About,
+  }
 ];
 
 const router = createRouter({
